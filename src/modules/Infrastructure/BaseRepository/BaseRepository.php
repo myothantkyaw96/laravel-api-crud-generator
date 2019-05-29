cd3641aa8ebfddf45a5ce189c32925099f6afa3e
Myo Thant Kyaw <myothantkyaw.bb19@gmail.com>
2019-05-29 17:39:21 +0630

    add infrastructure layer

---
+++ src/modules/Infrastructure/BaseRepository/BaseRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Module\Infrastructure\BaseRepository;
+
+/**
+ * @author Myo Thant Kyaw
+ * @copyright 2019 (c) Myo Thant Kyaw
+ *
+ * Class BaseRepository
+ * @package Module\Infrastructure\BaseRepository
+ */
+abstract class BaseRepository
+{
+
+}
---
+++ src/modules/Infrastructure/BaseValidator/BaseValidator.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Module\Infrastructure\BaseValidator;
+
+/**
+ * @author Myo Thant Kyaw
+ * @copyright 2019 (c) Myo Thant Kyaw
+ *
+ * Class BaseValidator
+ * @package Module\Infrastructure\BaseValidator
+ */
+abstract class BaseValidator
+{
+
+}
